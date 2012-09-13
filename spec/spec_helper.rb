32dd0cb285b9797cc1d3089adaa0219579b06161
Akshay S Murthy <akshay.s@multunus.com>
2012-09-13 09:20:27 +0530

    Removed unwanted files

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,4 @@
-require_relative '../resume_v1'
+require_relative '../resume'
 require_relative '../text_resume_writer'
 require_relative '../pdf_resume_writer'
 require_relative '../resume_factory'
